fd43ea25c82cac6f684b102b5e38574b6d8fb96f
MDAnarchie <115984118+MDAnarchie@users.noreply.github.com>
2024-06-20 11:50:17 +0200

    small fix Hero positioning

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,10 +22,10 @@ const Hero = () => {
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
       <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw]">
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
-          </h2>
+          </p>
           <TextGenerateEffect
             className="text-center text-[40px]md:text-5xl lg:text-6xl"
             words="Transforming Concepts into Seamless User Experiences"
